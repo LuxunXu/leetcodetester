352381673efcc3fbb6e1916d10430142c655bbf0
Luxun <lxu051@ucr.edu>
2019-07-22 12:23:18 -0700

    Q28

--- src/main/java/edu/ucr/cs/lxu051/GeneralTest.java
+++ src/main/java/edu/ucr/cs/lxu051/GeneralTest.java
@@ -1,6 +1,7 @@
 package edu.ucr.cs.lxu051;
 
 import edu.ucr.cs.lxu051.classes.TreeNode;
+import edu.ucr.cs.lxu051.solutions.Q28;
 import edu.ucr.cs.lxu051.solutions.Q56;
 import edu.ucr.cs.lxu051.utils.TreeUtils;
 
@@ -10,16 +11,19 @@ public class                                                                    
 //        Integer[] array = {3, 9, 20, null, null, 15, 7};
 //        TreeNode test = TreeUtils.buildTree(array);
 
-        String twoDArray = "1,3;2,6;8,10;15,18";
-        String[] tokens = twoDArray.split(";");
-        int[][] array = new int[tokens.length][tokens[0].split(",").length];
-        for (int i = 0; i < tokens.length; i++) {
-            String nums[] = tokens[i].split(",");
-            for (int j = 0; j < nums.length; j++) {
-                array[i][j] = Integer.parseInt(nums[j]);
-            }
-        }
-        Q56 q56 = new Q56();
-        q56.merge(array);
+//        String twoDArray = "1,3;2,6;8,10;15,18";
+//        String[] tokens = twoDArray.split(";");
+//        int[][] array = new int[tokens.length][tokens[0].split(",").length];
+//        for (int i = 0; i < tokens.length; i++) {
+//            String nums[] = tokens[i].split(",");
+//            for (int j = 0; j < nums.length; j++) {
+//                array[i][j] = Integer.parseInt(nums[j]);
+//            }
+//        }
+//        Q56 q56 = new Q56();
+//        q56.merge(array);
+
+        Q28 q28 = new Q28();
+        System.out.println(q28.kmp("aabaaabaaabaabaaac", "AAACAAAA"));
     }
 }
--- src/main/java/edu/ucr/cs/lxu051/solutions/Q28.java
+++ src/main/java/edu/ucr/cs/lxu051/solutions/Q28.java
@@ -11,13 +11,49 @@ public class Q28 {
 
     public int kmp(String s, String p) {
         int[] ips = preprocess(p);
-        return 0;
+        return match(s, p, ips);
     }
 
     public int[] preprocess(String p) {
         int[] ips = new int[p.length()];
-        int i = 0, j = 0;
-        
+        int i = 0, j = 1;
+        while (j < p.length()) {
+            if (p.charAt(j) == p.charAt(i)) {
+                ips[j] = i + 1;
+                j++;
+                i++;
+            } else {
+                if (i == 0) {
+                    j++;
+                } else {
+                    i = ips[i - 1];
+                }
+            }
+        }
         return ips;
+    }
+
+    public int match(String s, String p, int[] ips) {
+        int i = 0, j = 0;
+        while (j < s.length()) {
+            if (i == p.length()) {
+                return j - i;
+            }
+            if (s.charAt(j) == p.charAt(i)) {
+                j++;
+                i++;
+            } else {
+                if (i == 0) {
+                    j++;
+                } else {
+                    i = ips[i - 1];
+                }
+            }
+        }
+        if (i == p.length()) {
+            return j - i;
+        } else {
+            return -1;
+        }
     }
 }
